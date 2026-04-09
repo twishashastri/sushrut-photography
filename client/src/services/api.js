6c69bf99c69902b866a55afa7627b62f0b763aab
twishashastri <twishashastri@gmail.com>
2026-04-09 09:55:29 -0600

    Use environment variable for API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const API = axios.create({ 
-  baseURL: 'http://localhost:5000/api' 
+  baseURL: API_URL
 });
 
 API.interceptors.request.use((req) => {
